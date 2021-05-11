7a00c06538725ab6d53fa7f93690a8282105747d
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2021-05-11 19:49:19 +0200

    Function changing template for insta_bot

--- static/main.js
+++ static/main.js
@@ -16,7 +16,24 @@ function change_menu() {
     }
     
     title.style.color = my_color;
-    for (a = 0; a < strips.length; a++) {
+    for (var a = 0; a < strips.length; a++) {
         strips[a].style.backgroundColor = my_color;
     }
+}
+
+function to_share(){
+    document.getElementsByTagName('svg')[0].remove();
+    document.getElementById('container').remove();
+    naglowek = document.styleSheets[0].cssRules[1].style;
+    naglowek.removeProperty('margin');
+    document.styleSheets[0].addRule("#nagłówek", "margin: 2px");
+    document.getElementsByTagName("button")[0].remove() ;
+
+    title = document.getElementById("nagłówek");
+    dt = document.getElementById("story-title");
+    dd = document.getElementById('story-content');
+
+    title.style.fontSize = "50px"
+    dd.style.fontSize = "27px"
+    dt.style.fontSize = "37px"
 }
