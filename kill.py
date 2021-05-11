d5d694d968b00699c9e6c80de32b9fcb4b68f083
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2021-05-11 21:28:36 +0200

    Updated link to Instagram

--- kill.py
+++ kill.py
@@ -3,8 +3,9 @@ import os, time
 def kill_process():
     '''Restarts program'''
     with open("pid.txt", "r") as f:
-        pid = f.read()
+        pids = f.read().split('\n')
         f.close()
-    os.system(f"kill {pid}")
+    for pid in pids:
+        os.system(f"kill {pid}")
     time.sleep(3)
     os.system("python3 main.py")
