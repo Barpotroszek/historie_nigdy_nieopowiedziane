bcd34f09fcc6fb54b20f4c469eb0712bda27f842
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2021-05-24 23:20:58 +0200

    Repaired error

--- write_json.py
+++ write_json.py
@@ -48,7 +48,7 @@ class templates:
                 category = 'updates'
             var['title'] = title
             now_time = datetime.datetime.now().strftime('%d.%m.%Y')
-            var['time'] = now_time
+            var['date'] = now_time
             var['category'] = category
             var['saved'] = "False"
             var['content'] = content
